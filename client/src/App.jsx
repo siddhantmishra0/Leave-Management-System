d73349a5ac2b26098d5da00b400283180f18ad09
Dhanraj Shetty <darshitdhanrajshetty@gmail.com>
2025-07-25 07:47:53 -0700

    new ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import RequestLeave from "./pages/RequestLeave"; // Adjust if in components
-
+import { Gemini } from "./components/components";
 const App = () => (
   <div className="app">
     <Navbar />
     <Routes>
       <Route path="/request-leave" element={<RequestLeave />} />
+      <Route path="/gemini" element={<Gemini />} />
     </Routes>
   </div>
 );
--- client/src/components/components.js
+++ client/src/components/components.js
@@ -3,4 +3,5 @@ import Reason from "./Reason.jsx";
 import ReasonTextarea from "./ReasonTextarea.jsx";
 import SubmitButton from "./SubmitButton.jsx";
 import LeaveCard from "./LeaveCard.jsx";
-export { Calendar, LeaveCard, ReasonTextarea, Reason, SubmitButton };
+import Gemini from "../gemini.jsx";
+export { Calendar, Gemini, LeaveCard, ReasonTextarea, Reason, SubmitButton };
